fcb379701724c5f196370ae3cb452dcb383c7c4e
Darshani <dbarasker@systango.com>
2022-12-19 18:11:43 +0530

    Figma-new template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatmoduleModule } from './shared/matmodule/matmodule.module';
+import { ContentComponent } from './main/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatmoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
