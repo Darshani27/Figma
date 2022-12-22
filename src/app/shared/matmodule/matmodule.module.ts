679bd3ded12f10cff56edbb63adae49236a07395
Darshani <dbarasker@systango.com>
2022-12-22 18:06:01 +0530

    Added changes for content page

--- src/app/shared/matmodule/matmodule.module.ts
+++ src/app/shared/matmodule/matmodule.module.ts
@@ -4,11 +4,11 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
 
 
 
-
-const modules=[MatToolbarModule,MatIconModule,MatButtonModule,MatCardModule]
+const modules=[MatToolbarModule,MatIconModule,MatButtonModule,MatCardModule,MatInputModule]
 
 @NgModule({
   declarations: [],
