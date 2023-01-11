562bc6ab599d1429a1b19219796488e7187fa485
Darshani27 <80616303+Darshani27@users.noreply.github.com>
2023-01-11 23:42:20 +0530

    Update footer.component.ts

--- src/app/footer/footer/footer.component.ts
+++ src/app/footer/footer/footer.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 // https://stackoverflow.com/questions/45437092/rotate-custom-marker-google-maps-api
 //   https://stackoverflow.com/questions/23149613/change-google-map-marker-orientation-according-path-direction
-//   https://stackoverflow.com/questions/6800613/rotating-image-marker-image-on-google-map-v3
+// //   https://stackoverflow.com/questions/6800613/rotating-image-marker-image-on-google-map-v3
+//   https://balramchavan.medium.com/display-and-track-users-current-location-using-google-map-geolocation-in-angular-5-c259ec801d58
   constructor() { }
 
   ngOnInit(): void {
